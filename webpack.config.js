603735b933ccc27ff58e0860a8c3b059d0258eff
Matthew Alner <matthew.alner@senseye.io>
2022-07-08 16:06:13 +0100

    Fix Lib name in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: './src/thumbtackstic.js',
   output: {
-    library: 'thumbtackstic',
+    library: 'Thumbtackstic',
     libraryTarget: 'umd',
     libraryExport: 'default',
     filename: 'thumbtackstic.min.js',
